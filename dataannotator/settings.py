f5fdb6955d1e5470f2a4c7cedef82c1a90a85712
Jue Zhao <nzjerryzhao@gmail.com>
2021-04-25 15:26:03 +1200

    1

--- dataannotator/settings.py
+++ dataannotator/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don"t run with debug turned on in production!
 if sys.argv[1] == "runserver":
-    DEBUG = False
+    DEBUG = True
 else:
     DEBUG = False
 
